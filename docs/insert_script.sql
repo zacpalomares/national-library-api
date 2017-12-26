75847f50b2356144ed087cbd39839a862cdf2127
zacpalomares <isaac.daniel.palomares@gmail.com>
2017-12-26 23:49:54 +0800

    Added new users/authors for script

--- docs/insert_script.sql
+++ docs/insert_script.sql
@@ -1,6 +1,12 @@
 -- for default users
-insert into users(username, password, is_super_user) values('admin', 'pass', true);
-insert into users_info(first_name, last_name, user_id) VALUES('fAdmin', 'lAdmin', 1);
+insert into users(username, password, "role") values('admin', 'pass', 'ADMIN');
+insert into users_info(author_name, author_country, user_id) VALUES('fAdmin', 'USA', 1);
 
-insert into users(username, password, is_super_user) values('author', 'pass', false);
-insert into users_info(first_name, last_name, user_id) VALUES('fAuthor', 'lAuthor', 2);
+insert into users(username, password, "role") values('author', 'pass', 'AUTHOR');
+insert into users_info(author_name, author_country, user_id) VALUES('fAuthor', 'United Kingdom', 2);
+
+insert into users(username, password, "role") values('author2', 'pass2', 'AUTHOR');
+insert into users_info(author_name, author_country, user_id) VALUES('fAuthor2', 'Pakistan', 3);
+
+insert into users(username, password, "role") values('author3', 'pass3', 'AUTHOR');
+insert into users_info(author_name, author_country, user_id) VALUES('fAuthor3', 'India', 4);
